76e186dc8267e4bfba8d832b20e8a7765281ec21
m.krzyszczyk <marek.krzyszczyk@wp.pl>
2021-01-21 20:35:42 +0100

    task 5 - 0-3

--- Interview/Interview/Task5/ConsoleCountriesSearchReportWriter.cs
+++ Interview/Interview/Task5/ConsoleCountriesSearchReportWriter.cs
@@ -10,9 +10,21 @@ namespace Interview.Task5
 
             foreach (var country in foundCountries)
             {
-                Console.WriteLine($"- {country.Name} ze stolicą w {country.Capital}.");
-                Console.WriteLine($"  - ludność: {country.Population / 1e6} milionów");
-                Console.WriteLine($"  - powierzchnia: {country.Area} km^2");
+                string countryName = country.Name;
+                string countryCapitalLetters = countryName.ToUpper();
+                int density = (int) (country.Population / country.Area);
+
+                Console.WriteLine($"- {countryCapitalLetters}");
+                Console.WriteLine($"  - stolica: {country.Capital}");
+                Console.WriteLine($"    - ludność: {country.Population / 1e6} milionów");
+                Console.WriteLine($"    - powierzchnia: {country.Area} km^2");
+
+                Console.WriteLine($"  - stolica: {country.Capital}");
+                Console.WriteLine($"    - ludność: {country.Population / 1e6} milionów");
+                Console.WriteLine($"    - powierzchnia: {country.Area} km^2");
+                Console.WriteLine($"    - długość i szerokość geograficzna: {country.LatLng[0]} i {country.LatLng[1]} stopni");
+                Console.WriteLine($"    - gęstość zaludnienia: {density} osoby/km^2");
+                Console.WriteLine($"    - alpha3Code: {country.Alpha3Code}");
             }
         }
     }
